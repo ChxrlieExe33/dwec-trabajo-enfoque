ad54caa6c696012920d0f6222ff41b4d379143c3
Charlie <charliedavidcrane13@gmail.com>
2025-11-30 17:23:46 +0100

    Creado el footer de la página principal como componente.

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {HeaderBarComponent} from '../../components/header-bar/header-bar.component';
 import {MainPageOptionsComponent} from '../../components/main-page-options/main-page-options.component';
+import {FooterBarComponent} from '../../components/footer-bar/footer-bar.component';
 
 @Component({
   selector: 'app-main-page',
   imports: [
     HeaderBarComponent,
-    MainPageOptionsComponent
+    MainPageOptionsComponent,
+    FooterBarComponent
   ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
